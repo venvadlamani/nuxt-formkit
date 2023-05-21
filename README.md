9913f153877dd97f9ee1de7ca932c43aa709971f
venvadlamani <122305594+venvadlamani@users.noreply.github.com>
2023-05-21 07:07:48 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nuxt-formkit
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/nuxt-starter-mynjej)
---
+++ layouts/default.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div class="bg-gray-100 dark:bg-gray-800">
+      <div
+        class="
+          max-w-4xl
+          flex flex-col
+          min-h-screen
+          px-4
+          mx-auto
+          overflow-x-hidden
+          sm:px-10
+        "
+      >
+        <header class="flex-none sm:px-12">
+          <NavBar />
+        </header>
+
+        <main class="flex-grow sm:px-12">
+          <slot />
+        </main>
+      </div>
+    </div>
+  </div>
+</template>
