5272aa410910ff1a9a5a9e3ab8863503cd313a4c
venvadlamani <122305594+venvadlamani@users.noreply.github.com>
2023-05-21 12:47:47 -0500

    1 file modified

--- formkit.config.ts
+++ formkit.config.ts
@@ -18,12 +18,12 @@ export default {
           'formkit-disabled:bg-gray-200 formkit-disabled:cursor-not-allowed formkit-disabled:pointer-events-none',
         input:
           'appearance-none bg-transparent focus:outline-none focus:ring-0 focus:shadow-none ',
-        label: 'block mb-2 font-bold text-sm',
+        label: 'block mb-1 font-bold text-sm',
         legend: 'font-bold text-sm',
         loaderIcon: 'inline-flex items-center w-4 text-gray-600 animate-spin',
         message: 'text-red-500 mb-1 text-xs',
         messages: 'list-none p-0 mt-1 mb-0',
-        outer: 'mb-4 formkit-disabled:opacity-50',
+        outer: 'mt-2 mb-4 formkit-disabled:opacity-50',
         prefixIcon:
           'w-10 flex self-stretch grow-0 shrink-0 rounded-tl rounded-bl border-r border-gray-400 bg-white bg-gradient-to-b from-transparent to-gray-200 [&>svg]:w-full [&>svg]:max-w-[1em] [&>svg]:max-h-[1em] [&>svg]:m-auto',
         suffixIcon:
